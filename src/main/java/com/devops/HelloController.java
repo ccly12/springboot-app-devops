9e4ec6e3b19587c013d13201eb3cf3d7d22a3726
ccly12 <claribelloperez02@gmail.com>
2026-04-09 15:13:20 -0600

    actulizacion

--- src/main/java/com/devops/HelloController.java
+++ src/main/java/com/devops/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "App funcionando 🚀";
+        return "FUNCIONANDO🚀";
     }
 
     @GetMapping("/saludo")
