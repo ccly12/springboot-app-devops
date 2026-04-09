a5c11f2d85efb34d4a8d9ed6b4f3e5bc3bd58edb
ccly12 <claribelloperez02@gmail.com>
2026-04-08 18:16:46 -0600

    commit

--- src/main/java/com/devops/HelloController.java
+++ src/main/java/com/devops/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "App funcionando sencillamente 🚀";
+        return "App funcionando 🚀";
     }
 
     @GetMapping("/saludo")
