f28a8e82e42609dd21b5a8e68ffacc3034b5e176
ccly12 <claribelloperez02@gmail.com>
2026-04-09 21:27:34 -0600

    commit jekis

--- src/main/java/com/devops/HelloController.java
+++ src/main/java/com/devops/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "FUNCIONANDO aqui 🚀";
+        return "FUNCIONANDO 🚀";
     }
 
     @GetMapping("/saludo")
