1e94412d34b3a0368e4f68006b95a846e504b3f5
ccly12 <claribelloperez02@gmail.com>
2026-04-10 03:23:11 -0600

    coment

--- src/main/java/com/devops/HelloController.java
+++ src/main/java/com/devops/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
 
     @GetMapping("/saludo")
     public String saludo() {
-        return "Hola desde Spring Boot XDdd 👋";
+        return "Hola desde Spring Boot XDd 👋";
     }
 }
