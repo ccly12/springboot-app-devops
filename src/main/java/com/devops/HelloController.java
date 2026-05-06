bcfc37ed8d0c518fcb0dc6007aea9797d6721df4
ccly12 <claribelloperez02@gmail.com>
2026-05-06 02:37:54 -0600

    feat:  página actulizada

--- src/main/java/com/devops/HelloController.java
+++ src/main/java/com/devops/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/saludo")
     public String saludo() {
-        return "¡Hola!  API de Spring Boot está funcionando perfectamente. 👩‍💻";
+        return "¡Hola!  API de Spring Boot está funcionando perfectamente. ¡Bienvenido a mi proyecto DevOps!";
     }
 }
